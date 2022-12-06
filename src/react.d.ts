fc3d3bcabbd9bc3e1acd2ca2f0d532dd13f4d5e4
Giuseppe Cutuli <giuseppe@mono.studio>
2022-12-06 16:55:06 +0100

    chore: add more info for workaround

--- src/react.d.ts
+++ src/react.d.ts
@@ -1,6 +1,7 @@
 import { AriaAttributes, DOMAttributes } from 'react'
 
 // workaround to fix this problem: https://github.com/johnsoncodehk/volar/discussions/592
+// https://docs.astro.build/en/guides/typescript/#vue-components-are-mistakenly-typed-by-the-typesreact-package-when-installed
 declare module 'react' {
   interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
     class?: string
