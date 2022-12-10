b6aabbfe15bcb37519bd2ffc8112fc01ef416502
Giuseppe Cutuli <giuseppe@mono.studio>
2022-12-10 11:11:45 +0100

    feat: sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), vue()]
+  site: 'https://devfest22-astro.pages.dev',
+  integrations: [react(), tailwind(), vue(), sitemap()]
 });
